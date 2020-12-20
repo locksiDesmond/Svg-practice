91819bbaaaedcee1f8e07b463650829cbecc17c5
Adenola desmond <56209529+locksiDesmond@users.noreply.github.com>
2020-12-20 01:55:14 +0100

    feat: add create-animation and update onscroll slider

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -1,9 +1,9 @@
-const layers = gsap.timeline();
+const layers = gsap.timeline({ ease: 'easeIn' });
 layers
-  .from('.orange', { xPercent: -100 })
-  .from('.purple', { xPercent: 100 })
-  .from('.red', { yPercent: -100 })
-  .from('.blue', { yPercent: 100 });
+  .from('.layers .orange', { xPercent: -100 })
+  .from('.layers .purple', { xPercent: 100 })
+  .from('.layers .red', { yPercent: -100 })
+  .from('.layers .blue', { yPercent: 100 });
 
 ScrollTrigger.create({
   trigger: '.whole-page',
@@ -11,4 +11,22 @@ ScrollTrigger.create({
   animation: layers,
   scrub: true,
   pin: true,
+});
+
+const sections = gsap.utils.toArray('.con');
+
+const horizontal = gsap.timeline({});
+horizontal.from(sections, {
+  xPercent: -100 * (sections.length - 1),
+  stagger: 1,
+});
+
+ScrollTrigger.create({
+  trigger: '.horizontal-scroll',
+  scroller: '#app',
+  animation: horizontal,
+  scrub: true,
+  pin: true,
+  snap: 1 / (sections.length - 1),
+  // end: () => '+=' + document.querySelector('.horizontal-scroll').offsetWidth,
 });
